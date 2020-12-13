5d8c6bec3a373065186e40af8ec2a907763e7476
Irina <parakerta@mail.ru>
2020-12-13 17:36:48 +0300

    bugfix id error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,22 @@ import { BrowserModule } from '@angular/platform-browser';
 export class AppComponent {
   title = 'project11';
   detectors = [];
-  random(){
-      var tf =Boolean(Math.round(Math.random())); 
+  randomBL(){
+      let tf =Boolean(Math.round(Math.random())); 
       return tf;
     }
   ngOnInit() {
    for(let i= 1;i<=10;i++)
-      {this.detectors.push({id: i, name: 'Name'+i, status: this.random()});}
+      {this.detectors.push({id: i, name: 'Name'+i, status: this.randomBL()});}
       
     }
    create(NameInput){
+     let c = 0;
      let len =  this.detectors.length -1;
-     let c = this.detectors[len].id+1;
-    {this.detectors.push({id: c, name: NameInput.value, status: this.random()});}
+     if(this.detectors[len])
+     c = this.detectors[len].id+1;
+     else c = 1;
+    {this.detectors.push({id: c, name: NameInput.value, status: this.randomBL()});}
       NameInput.value ='';
    }
    delete(id_click){
